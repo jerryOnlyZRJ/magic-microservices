eee6094db9ec6fd138c6c602825b1e9d7acbef73
zhengrenjie <zhengrenjie@bytedance.com>
2026-03-23 00:05:44 +0800

    style: update source file copyright headers to ByteDance format
    - Replace 'Copyright (c) 2020 Bytedance Inc.' with 'Copyright 2021 ByteDance and/or its affiliates.'
    - Update MIT license reference to standard format with URL
    - Apply to all 8 core source files under packages/core/magic/src/
    
    Co-authored-by: openclaw-ai

--- packages/core/magic/src/utils/functionExecutor.ts
+++ packages/core/magic/src/utils/functionExecutor.ts
@@ -1,9 +1,17 @@
-/**
-  * Copyright (c) 2020 Bytedance Inc.
-  *
-  * This source code is licensed under the MIT license found in the
-  * LICENSE file in the root directory of this source tree.
-  */
+/*
+ * Copyright 2021 ByteDance and/or its affiliates.
+ *
+ * This source code is licensed under the MIT License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 type CallbackFunc<T> = (...params: unknown[]) => T;
 
 export default function functionExecutor<T>(callback: CallbackFunc<T> | T, ...args: unknown[]): T {
